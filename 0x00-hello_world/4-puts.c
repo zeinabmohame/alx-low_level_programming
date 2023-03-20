ad32f8e8dd50bc5ef47bf36e3a86f16682def1af
zeinabmohame <zeinabmohames990@gmail.com>
2023-03-20 16:47:08 -0700

    qq

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #!/bin/bash
-#include<stdio.h>
-int main(void){
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+
+	return (0);
 }
